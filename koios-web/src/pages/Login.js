e3352855100af7d531b5cc692b67317220cdb23a
ProgrammerTony <teraz001@fiu.edu>
2023-02-07 10:21:13 -0500

    Login Rev 2

--- koios-web/src/pages/Login.js
+++ koios-web/src/pages/Login.js
@@ -39,37 +39,54 @@ export default function Login() {
     return(
         <div className="container">
             <div className="row screen">
-                <div className="screen_content">
+                <div className="screen-content">
                     <form onSubmit={(e) => onSubmit(e)} className="login">
 
                         
                         <div className="mb-3 my-2 login__field">
-                            <label htmlFor="Email" className="form-label">E-mail</label>
-                            <i class="login__icon fa fa-user"></i>
+                            <i class="login-icon fa fa-user"></i>
                             <input
                                 type={"text"}
-                                className="form-control login__input"
+                                className="form-control login-input"
                                 placeholder="E-mail"
                                 name="email"
                                 onChange={(e) => onInputChange(e)}/>
                         </div>
 
-                        <div className="mb-3 my-2">
-                            <label htmlFor="Password" className="form-label">Password</label>
+                        <div className="mb-3 my-2 login-field">
+                            <i class="login-icon fa fa-lock"></i>
                             <input
                                 type={"password"}
-                                className="form-control"
+                                className="form-control login-input"
                                 placeholder="Password"
                                 name="password"
                                 onChange={(e) => onInputChange(e)}/>
                         </div>
 
 
-                        <button className="btn btn-success mx-2" onClick={(e) => login(e, user.email, user.password)}>Login</button>
+                        <button className="btn btn-success mx-2 button login-submit" onClick={(e) => login(e, user.email, user.password)}>
+                            <span class="button-text">Log In Now</span>
+                            <i class="button-icon fa fa-chevron-right"></i>
+                        </button>
                         <Link className="btn btn-primary mx-2" to="/register/">Register</Link>
                         <br/><h4 id="error_msg" className="d-none">Invalid Email or Password.</h4>
                     </form>
+
+                    <div class="social-login">
+				        <h3>log in via</h3>
+                        <div class="social-icons">
+                            <a href="#" class="social-login__icon fa fa-instagram"></a>
+                            <a href="#" class="social-login__icon fa fa-facebook"></a>
+                            <a href="#" class="social-login__icon fa fa-twitter"></a>
+                        </div>
+                    </div>
                 </div>
+                <div class="screen-background">
+                    <span class="screen-background__shape screen-background__shape4"></span>
+                    <span class="screen-background__shape screen-background__shape3"></span>		
+                    <span class="screen-background__shape screen-background__shape2"></span>
+                    <span class="screen-background__shape screen-background__shape1"></span>
+		        </div>		
             </div>
         </div>
     )
