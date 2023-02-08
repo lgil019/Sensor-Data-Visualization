b8db936a3a6fb7fd7138bd537e77e98c92cbf10a
Gentman Tan <gentmantan@gmail.com>
2023-02-08 16:18:56 -0500

    Add bar graph functionality

--- koios-visualization/plot.py
+++ koios-visualization/plot.py
@@ -13,13 +13,25 @@ def plot_line(data):
     # Create a line plot using Bokeh
     p = figure(title="Line Plot", x_axis_label='x', y_axis_label='y')
     p.line(x, y)
-    # output_notebook()
+    output_notebook()
     output_file("line_plot.html")
     show(p)
 
+def plot_bar(data):
+    # Extract x and y values from the input data
+    x = [d['x'] for d in data]
+    y = [d['y'] for d in data]
+
+    # Create a line plot using Bokeh
+    p = figure(title="Bar Plot", x_axis_label='x', y_axis_label='y')
+    p.vbar(x=x, top=y, width=0.5)
+    output_notebook()
+    output_file("bar_graph.html")
+    show(p)
 
 # Read the JSON data from standard input
 data = json.loads(input())
 
 # Plot the data
-plot_line(data)
+# plot_line(data)
+plot_bar(data)
