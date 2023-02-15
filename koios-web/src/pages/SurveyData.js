4f66973b2b1aaf61dd434a4d715e5fc98c979c56
lgil019 <77906774+lgil019@users.noreply.github.com>
2023-02-14 19:37:28 -0500

    Revert "Merge branch 'main' of https://github.com/lgil019/Sensor-Data-Visualization"
    This reverts commit 0c9addda87e7ce24b05a37b279787e27bf6e21f9, reversing
    changes made to eb80b7ff3ef142a19657d27d834712c44afefca4.

--- koios-web/src/pages/SurveyData.js
+++ koios-web/src/pages/SurveyData.js
@@ -4,11 +4,13 @@ import {useState} from "react";
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
 import Dropdown from 'react-bootstrap/Dropdown';
 import DropdownButton from 'react-bootstrap/DropdownButton';
+import mysql from "mysql";
 
 export default function surveyData() {
 
+
   return (
-  <>
+    <>
     <DropdownButton id="dropdown-basic-button" title="Surveys">
       <Dropdown.Item href="#/action-1">Survey 1</Dropdown.Item>
       <Dropdown.Item href="#/action-2">Survey 2</Dropdown.Item>
@@ -21,6 +23,7 @@ export default function surveyData() {
       <Dropdown.Item href="#/action-3">Version 3</Dropdown.Item>
     </DropdownButton>
 
+
  <DropdownButton id="dropdown-basic-button3" title="Time Interval">
       <Dropdown.Item href="#/action-1">Monthly</Dropdown.Item>
       <Dropdown.Item href="#/action-2">Weekly</Dropdown.Item>
@@ -28,11 +31,13 @@ export default function surveyData() {
     </DropdownButton>
 </>
   );
-}
+
+  }
+
 
     
 
-
+    
     
     
     
