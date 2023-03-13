2793d2550713081271304171be4ba5aed569cc10
ProgrammerTony <teraz001@fiu.edu>
2023-03-13 00:42:24 -0400

    Rest API Update
    REST api that has the users percentage for your time awake and you need to know when you were asleep do a combined both for now it will be a fraction of how much time awake out of 24 hours have it available as a mobile end point

--- koios-server/src/main/java/com/koios/server/repository/QuestionsRepository.java
+++ koios-server/src/main/java/com/koios/server/repository/QuestionsRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import com.koios.server.model.Question;
+
 public interface QuestionsRepository extends JpaRepository<Question, Long> {
 
 	@Query(value = "SELECT * FROM survey_task q WHERE q.survey_id = ?1", nativeQuery = true)
